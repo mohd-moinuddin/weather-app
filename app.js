ffb1d64edaed756305fefa388a62c38279f21718
MOHD MOINUDDIN <imoin135@gmail.com>
2022-07-05 16:39:50 +0530

    static html files

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
-const { response } = require('express');
-const request = require('request');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const publicDirectoryPath = path.join(__dirname,'/public')
+
+app.use(express.static(publicDirectoryPath));
 
 
-const weatherURL = 'http://api.weatherstack.com/current?access_key=8e187bb7a3480df0435831105d7d51ca&query=39.8267,-122.4233'
-const geocodeURL = 'http://api.ipstack.com/1000:4052:e15:f885:f834:25fc:4d49:ab4?access_key=a5554fc431358a018fb1ba414fea60b4&format=1'
-
-request({ url: weatherURL, json: true }, (error, response) => {
-
-    if(error){
-        console.log('Error occured')
-    }else if(response.body.error){
-        console.log('unable to find location.')
-    }else{ 
-         console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degress out. It feels like ' + response.body.current.feelslike + ' degress.')
-
-    }
-    
-})  
-
-
-request({url:geocodeURL,json:true},(error,response)=>{
-       
-        if(error){
-            console.log('Unable to connect with location services')
-        }else if(response.body.location.length===0){
-            console.log('unable to find location . Try another ')
-        }else{
-            const latitude = response.body.latitude
-            const longitude = response.body.longitude
-            console.log(latitude,longitude);}
+app.get('',(req,res)=>{
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
 
+app.get('/help',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/public/help.html'))
+})
+
+app.get('/about',(req,res)=>{
+    res.send('<h1>About</h1>')
+})
+
+app.get('/weather',(req,res)=>{
+    res.send('<h1>Your weather </h1>')
+})
+
+
+app.listen(3000,()=>{
+    console.log('Server is running on PORT');
+})
