959a4f26d53d5b97cbb780ab3f53f6f61be6cc7c
MOHD MOINUDDIN <imoin135@gmail.com>
2022-07-08 22:56:57 +0530

    Templating final

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const express = require('express');
-
+const hbs = require('hbs');
 const app = express();
 
 //Define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'/public')
-const viewsPath           = path.join(__dirname,'../templates');
+const viewsPath           = path.join(__dirname,'../templates/views');
+const partialsPath         = path.join(__dirname,'../templates/partials');
 
 //Setup handlebars engine and views location
 app.set('view engine','hbs');
 app.set('views' , viewsPath);
+hbs.registerPartials(partialsPath);
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
@@ -41,7 +43,11 @@ app.get('/weather',(req,res)=>{
    res.send('Hi i am weather');
 })
 
-
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:'I AM 404'
+    })
+ })
 app.listen(3000,()=>{
     console.log('Server is running on PORT');
 })
